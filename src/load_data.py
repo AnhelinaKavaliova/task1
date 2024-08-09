1954d9c443182f61bbe64ff5c8739424c0f656b2
Your Name <angelinakovaleva228@gmail.com>
2024-08-09 10:18:01 +0300

    add unit test for load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,42 +1,55 @@
 import json
 from typing import List, Tuple
-
+import logging
 from db_manager import DBManager
 
+logger = logging.getLogger("load_data")
 
 class LoadData:
     def __init__(self, db_manager: DBManager) -> None:
         self.db_manager = db_manager
 
     def load_data_rooms(self, rooms_path: str) -> None:
-        with open(rooms_path) as f:
-            rooms: List[dict] = json.load(f)
+        try:
+            with open(rooms_path) as f:
+                rooms: List[dict] = json.load(f)
 
-        data: List[Tuple[int, str]] = [(room["id"], room["name"]) for room in rooms]
+            data: List[Tuple[int, str]] = [(room["id"], room["name"]) for room in rooms]
 
-        query = "INSERT INTO rooms(id, name) VALUES (%s, %s)"
+            query = "INSERT INTO rooms(id, name) VALUES (%s, %s)"
 
-        self.db_manager.execute_many_query(query, data)
+            self.db_manager.execute_many_query(query, data)
+            logger.info("Successfully loaded rooms into the database")
+        except FileNotFoundError as err:
+            logger.error(f"Error: {err}")
+        except Exception as err:
+            logger.error(f"Error: {err}")
 
     def load_data_students(self, students_path: str) -> None:
-        with open(students_path) as f:
-            students: List[dict] = json.load(f)
+        try:
+            with open(students_path) as f:
+                students: List[dict] = json.load(f)
 
-        data: List[Tuple[int, str, str, int, str]] = [
-            (
-                student["id"],
-                student["name"],
-                student["birthday"],
-                student["room"],
-                student["sex"],
+            data: List[Tuple[int, str, str, int, str]] = [
+                (
+                    student["id"],
+                    student["name"],
+                    student["birthday"],
+                    student["room"],
+                    student["sex"],
+                )
+                for student in students
+            ]
+            query = (
+                """
+                INSERT INTO students(id, name, birthday, room, sex)
+                VALUES (%s, %s, %s, %s, %s)
+                """
             )
-            for student in students
-        ]
-        query = (
-            """
-            INSERT INTO students(id, name, birthday, room, sex)
-            VALUES (%s, %s, %s, %s, %s)
-            """
-        )
 
-        self.db_manager.execute_many_query(query, data)
+            self.db_manager.execute_many_query(query, data)
+            logger.info("Successfully loaded students into the database")
+        except FileNotFoundError as err:
+            logger.error(f"Error: {err}")
+        except Exception as err:
+            logger.error(f"Error: {err}")
---
+++ tests/test_load_data.py
@@ -0,0 +1,92 @@
+import pytest
+from src.load_data import LoadData
+from src.db_manager import DBManager
+from unittest.mock import *
+import json
+
+@pytest.fixture
+def db_manager_mock():
+    yield Mock(DBManager)
+
+@pytest.fixture
+def load_data(db_manager_mock):
+    yield LoadData(db_manager_mock)
+
+@pytest.fixture
+def mock_logger():
+    with patch('src.load_data.logger') as mock_logger:
+        yield mock_logger
+
+def test_load_data_rooms_success(db_manager_mock, load_data, mock_logger):
+    data = mock_open(read_data=json.dumps([
+            {"id": 1, "name": "Room #1"},
+            {"id": 2, "name": "Room #2"}
+        ]))
+    with patch('builtins.open', data):
+        load_data.load_data_rooms("path.json")
+
+    expected_query = "INSERT INTO rooms(id, name) VALUES (%s, %s)"
+    expected_data = [(1, "Room #1"), (2, "Room #2")]
+    db_manager_mock.execute_many_query.assert_called_once_with(expected_query, expected_data)
+    
+    mock_logger.info.assert_called_with("Successfully loaded rooms into the database")
+
+def test_load_data_rooms_file_not_found(db_manager_mock, load_data, mock_logger):
+    with patch('builtins.open', side_effect = FileNotFoundError("File not found")):
+        load_data.load_data_rooms("path.json")
+
+    mock_logger.error.assert_called_with("Error: File not found")
+
+def test_load_data_rooms_failed(db_manager_mock, load_data, mock_logger):
+    with patch('builtins.open', side_effect = Exception("Test exception")):
+        load_data.load_data_rooms("path.json")
+    
+    mock_logger.error.assert_called_with("Error: Test exception")
+
+def test_load_data_students_success(db_manager_mock, load_data, mock_logger):
+    data = mock_open(read_data=json.dumps([
+        {
+            "id": 1,
+            "name": "Luke",
+            "birthday": "2001-09-13",
+            "room": 101,
+            "sex": "M"
+        },
+        {
+            "id": 2,
+            "name": "Xto",
+            "birthday": "1999-05-15",
+            "room": 102,
+            "sex": "F"
+        }
+    ]))
+
+    with patch('builtins.open', data):
+        load_data.load_data_students("path.json")
+
+    expected_query = (
+                """
+                INSERT INTO students(id, name, birthday, room, sex)
+                VALUES (%s, %s, %s, %s, %s)
+                """
+            )
+
+    expected_data = [
+        (1, "Luke", "2001-09-13", 101, "M"),
+        (2, "Xto", "1999-05-15", 102, "F")
+    ]
+    db_manager_mock.execute_many_query.assert_called_once_with(expected_query, expected_data)
+    
+    mock_logger.info.assert_called_with("Successfully loaded students into the database")
+
+def test_load_data_students_file_not_found(db_manager_mock, load_data, mock_logger):
+    with patch('builtins.open', side_effect = FileNotFoundError("File not found")):
+        load_data.load_data_students("path.json")
+
+    mock_logger.error.assert_called_with("Error: File not found")
+
+def test_load_data_students_failed(db_manager_mock, load_data, mock_logger):
+    with patch('builtins.open', side_effect = Exception("Test exception")):
+        load_data.load_data_students("path.json")
+    
+    mock_logger.error.assert_called_with("Error: Test exception")
